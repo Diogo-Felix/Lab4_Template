d605a5e97d1d2d37f89f71cb4fe66dc832ca70f6
Diogo-Felix <undisclosed>
2020-12-15 18:13:37 +0000

    Laboratório nivel basico completo

--- src/com/pa/patterns/memento/view/GUI.java
+++ src/com/pa/patterns/memento/view/GUI.java
@@ -1,5 +1,6 @@
 package com.pa.patterns.memento.view;
 
+import com.pa.patterns.memento.model.NoMementoException;
 import com.pa.patterns.memento.model.Product;
 import com.pa.patterns.memento.model.ShoppingCartController;
 import javafx.application.Application;
@@ -53,7 +54,7 @@ public class GUI extends Application {
         buttonAddProduct.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                if (textFieldProductName.getText().isBlank() || textFieldPrice.getText().isBlank()) {
+                if (textFieldProductName.getText().isEmpty() || textFieldPrice.getText().isEmpty()) {
                     error("Missing product information.");
                 } else {
                     try {
@@ -82,7 +83,7 @@ public class GUI extends Application {
 
         comboBoxMemento = new ComboBox<>();
         comboBoxMemento.setPromptText("Select a time to restore");
-//        hBoxUndo.getChildren().add(comboBoxMemento);
+        hBoxUndo.getChildren().add(comboBoxMemento);
 
         Button buttonUndo = new Button("Undo");
         hBoxUndo.getChildren().add(buttonUndo);
@@ -96,6 +97,12 @@ public class GUI extends Application {
             @Override
             public void handle(ActionEvent actionEvent) {
                 // TODO
+                try {
+                    shoppingCartController.undo();
+                    updateProductCartList();
+                } catch (NoMementoException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
--- test/com/pa/patterns/memento/model/ShoppingCartControllerTest.java
+++ test/com/pa/patterns/memento/model/ShoppingCartControllerTest.java
@@ -2,6 +2,8 @@ package com.pa.patterns.memento.model;
 
 import org.junit.jupiter.api.*;
 
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -22,21 +24,36 @@ class ShoppingCartControllerTest {
 
     @Test
     void reset() {
+        ctrl.addProduct("Test product", 42.0);
+        ctrl.addProduct("Test product2", 43.0);
+        ctrl.reset();
+        assertEquals(0, ctrl.getProducts().size());
     }
 
     @Test
     void removeProduct() {
+        ctrl.addProduct("Test product", 42.0);
+        ctrl.removeProduct("Test product");
+        assertEquals(0, ctrl.getProducts().size());
     }
 
     @Test
-    void undo() {
+    void undo() throws NoMementoException {
+        ctrl.addProduct("Test product", 42.0);
+        ctrl.undo();
+        assertEquals(0, ctrl.getProducts().size());
     }
 
     @Test
     void getProducts() {
+        ctrl.addProduct("product test", 10);
+        Collection<Product> products = ctrl.getProducts();
+        assertEquals(products,ctrl.getProducts());
     }
 
     @Test
     void showAll() {
+        ctrl.addProduct("product test", 10);
+        assertEquals("[product test (10.0)]", ctrl.getProducts().toString());
     }
 }
